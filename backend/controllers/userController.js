e07c2e7ab22d43a87944d5cf8f98c244cd1ceac5
NorbLevi <norbtony2268@gmail.com>
2024-06-13 09:26:10 +0300

    updated modules

---
+++ backend/controllers/userController.js
@@ -0,0 +1,19 @@
+// backend/controllers/userController.js
+
+const User = require('../models/User');
+
+const createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const newUser = new User({ name, email, password });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
+// backend/server.js
+
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 
@@ -9,15 +12,16 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+// Use routes
+app.use('/api', userRoutes);
+
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-
-app.get('/', (req, res) => {
-  res.send('Backend is working');
-});
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
