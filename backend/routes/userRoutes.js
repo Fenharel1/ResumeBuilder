b8396527dc74b64754b6c3353a2e6b5656adfa26
reinhard <reyqsp@gmail.com>
2024-06-17 08:47:51 -0500

    solving conflicts

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 // backend/routes/userRoutes.js
 
 const express = require('express');
-const { createUser } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
+const { getUserProfile } = require('../controllers/userController');
 
 const router = express.Router();
 
-router.post('/users', createUser);
+// Protect this route
+router.get('/profile', protect, getUserProfile);
 
 module.exports = router;
