0c1f0fcb3de049d8caf290bc93b23e46b8dd8fba
NorbLevi <norbtony2268@gmail.com>
2024-06-20 05:58:55 +0300

    User Dashboard and Cv Tamplate

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -7,6 +7,7 @@ import { ResumeBuilder } from "../modules/Build/pages/ResumeBuilder";
 import Register from "../modules/Build/components/Register";
 import Login from "../modules/Build/components/Login";
 import OAuthCallback from "../modules/Build/components/OAuthCallback";
+import Dashboard from "../modules/Build/components/Dashboard";
 
 export const AppRoutes = () => {
   return (
@@ -26,6 +27,7 @@ export const AppRoutes = () => {
       <Route path="/home" element={<LandingPage></LandingPage>}></Route>
       <Route path="/register" element={<Register/>}/>
       <Route path="/login" element={<Login/>}/>
+      <Route path="/dashbord" element={<Dashboard/>}/>
       <Route path="/auth/callback" element={<OAuthCallback />} />
       <Route path="/*" element={<Navigate to="/home"></Navigate>}></Route>
     </Routes>
