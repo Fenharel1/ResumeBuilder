f0e63977a09db340d684222d4597c4d52afcad4b
NorbLevi <norbtony2268@gmail.com>
2024-06-20 06:01:42 +0300

    Updated Contollers, Middleware, routes And User.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,16 @@
 // backend/routes/userRoutes.js
 
 const express = require('express');
-const { protect } = require('../middleware/authMiddleware');
-const { getUserProfile } = require('../controllers/userController');
-
 const router = express.Router();
+const { getUserProfile, getCvTemplates, uploadCv, getUserCvs } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
+const multer = require('multer');
 
-// Protect this route
-router.get('/profile', protect, getUserProfile);
+const upload = multer({ dest: 'uploads/' });
+
+router.route('/profile').get(protect, getUserProfile);
+router.route('/cv-templates').get(protect, getCvTemplates);
+router.route('/cvs').get(protect, getUserCvs);
+router.route('/upload-cv').post(protect, upload.single('cv'), uploadCv);
 
 module.exports = router;
